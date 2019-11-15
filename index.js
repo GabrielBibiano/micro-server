2e2021b7f6c5e8aa31b979c76db3fdd6a7c1e49a
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2019-11-15 02:21:54 -0300

    add: process.env.PORT

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/:component', async (req, res) => {
   }
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`APP LISTEN IN PORT ${PORT}`)
 })
